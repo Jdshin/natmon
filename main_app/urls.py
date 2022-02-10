5c1efdf8ab9176153078e31184b41a50992f0316
James Shin <jddshin@gmail.com>
2022-02-09 23:09:46 -0500

    Add About and Plants route with prelim views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    path('about', views.About.as_view(), name='about'),
+    path('plants', views.PlantList.as_view(), name='plants'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView, CreateView, UpdateView, DeleteVie
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
+    
+class About(TemplateView):
+    template_name = 'about.html'
+    
+class PlantList(TemplateView):
+    template_name = 'plantList.html'
