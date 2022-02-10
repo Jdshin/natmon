98f77f5b570b60f73e40517892505fce1b8163fe
James Shin <jddshin@gmail.com>
2022-02-10 00:26:56 -0500

    Create plant model and migrate database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    common_name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
