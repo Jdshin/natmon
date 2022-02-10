20236c09855198291a861428c61ca3d9bf1e445c
James Shin <jddshin@gmail.com>
2022-02-10 16:13:23 -0500

    Add User model, add login path/view, add navbar to _base.html

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Plant(models.Model):
@@ -8,4 +9,4 @@ class Plant(models.Model):
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about', views.About.as_view(), name='about'),
-    path('plants', views.PlantList.as_view(), name='plant_list'),
+    path('about/', views.About.as_view(), name='about'),
+    path('plants/', views.PlantList.as_view(), name='plant_list'),
     path('plants/new', views.PlantCreate.as_view(), name='plant_create'),
     path('plants/<int:pk>', views.PlantDetail.as_view(), name='plant_detail'),
     path('plants/<int:pk>/update', views.PlantUpdate.as_view(), name='plant_update'),
     path('plants/<int:pk>/delete', views.PlantDelete.as_view(), name='plant_delete'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
